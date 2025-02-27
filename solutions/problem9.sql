a445aee8f5dfb76b835b67db5f4e02c1a1f7a49f
Caleb Cook <bmxcaleb@gmail.com>
2025-02-26 21:36:30 -0600

    formatting

--- solutions/problem9.sql
+++ solutions/problem9.sql
@@ -1,1 +1,5 @@
-select department, count(worker_id) as Workers from worker where joining_date>"2014-04-01" group by department order by Workers DESC;
+select department, count(worker_id) as Workers 
+from worker 
+where joining_date>"2014-04-01" 
+group by department 
+order by Workers DESC;
